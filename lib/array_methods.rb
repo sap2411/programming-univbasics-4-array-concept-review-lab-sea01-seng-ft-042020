942854797be4436418bacb621864b4bf0eb5b184
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-17 05:43:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,10 +21,10 @@ def find_max_value(array)
      q = array[counter]
      u += 1 
   else array[counter] < array[counter+u]
-    array[counter+]
+    q = array[counter+u]
     counter +=1
   end
-  p
+  p q
 end
 
 def find_min_value(array)
