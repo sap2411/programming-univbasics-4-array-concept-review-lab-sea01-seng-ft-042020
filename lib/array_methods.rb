c5b7ede3f24709aedea5c39c59dfb9f392fa040e
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-17 05:10:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   counter = 0 
   while array[counter] do
   if (value_to_find = array[counter])
-    puts counter
+    p counter
     counter +=1
   else ()
    puts nil
